e2fcaddad4b8690ebccda9551b3fb6d1000c0109
ASUSNB <yeksancansu@gmail.com>
2019-04-19 15:39:53 +0300

    Merge branch 'master' of https://github.com/cyeksan/TennisTournament
    # Conflicts:
    #	.idea/misc.xml

--- app/src/main/java/com/example/asusnb/tennistournament/model/ListModel.java
+++ app/src/main/java/com/example/asusnb/tennistournament/model/ListModel.java
@@ -1,4 +1,22 @@
 package com.example.asusnb.tennistournament.model;
 
 public class ListModel {
+    private Player firstPlayer;
+    private Player secondPlayer;
+
+    public Player getFirstPlayer() {
+        return firstPlayer;
+    }
+
+    public void setFirstPlayer(Player firstPlayer) {
+        this.firstPlayer = firstPlayer;
+    }
+
+    public Player getSecondPlayer() {
+        return secondPlayer;
+    }
+
+    public void setSecondPlayer(Player secondPlayer) {
+        this.secondPlayer = secondPlayer;
+    }
 }
